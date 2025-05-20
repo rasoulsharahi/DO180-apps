b79781c3a2a141bd51189da20092be8485c23c49
rasoulsharahi <33823735+rasoulsharahi@users.noreply.github.com>
2025-05-20 14:43:04 +0200

    Update2 index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,161 +1,70 @@
 <!DOCTYPE html>
-<?php
-//echo 'Hello World.';
-// This is a one-line c++ style comment
-    /* This is a multi line comment
-       yet another line of comment */
-	   
-//phpinfo();
-
-//echo 'Hello World!!!/n Below is a basic calculator I made!';
-echo '<span style="color:#196719;text-align:center;">Hello World!. Below is a basic calculator I made!</span>';
-//echo  <span style="color:#AFA;text-align:center;">nl2br ("Hellow World! \n Below is a basic calculator I made!")</span>;
-
-ini_set('display_errors',0);
-	
-	if(isset($_POST['submit']))
-	{
-		$num1=$_POST['number1'];
-		$num2=$_POST['number2'];
-		if(is_numeric['$num1'] && is_numeric['$num2'])
-		{
-			if(isset($_POST['g']))
-			{
-				$operation=$_POST['g'];
-				
-				switch($operation)
-				{
-					case '+' :
-					$result=$num1+$num2;
-					break;
-					
-					case '-' :
-					$result=$num1-$num2;
-					break;
-					
-					case '*' :
-					$result=$num1*$num2;
-					break;
-					
-					
-					case '/' :
-					$result=$num1/$num2;
-					break;
-					
-					case '%' :
-					$result=$num1%$num2;
-					break;
-				}
-				
-				
-			}
-			else
-			{
-				echo "Please select operation";
-			}
-			
-		}
-		else
-		{
-			echo "Please enter numeric values only";
-		}
-	}
-?>
-
 <html>
-<head>
-<style> 
-#group {
-	animation-fill-mode: forwards; /*animation will stop at last frame and won't reset to start!!!*/
-    -webkit-animation: mymove 5s 1; /* Safari 4.0 - 8.0 */
-    animation: mymove 5s 1;
-	  position: absolute;
-       left: 5px;
-}
-
-/*#get {
+	<head>
+		<title>Calculator</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		 
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+	</head>
+	<body>
+		
+		<div class="container" style="margin-top: 50px">
+		
+			<?php
+			
+				// If the submit button has been pressed
+				if(isset($_POST['submit']))
+				{
+					// Check number values
+					if(is_numeric($_POST['number1']) && is_numeric($_POST['number2']))
+					{
+						// Calculate total
+						if($_POST['operation'] == 'plus')
+						{
+							$total = $_POST['number1'] + $_POST['number2'];	
+						}
+						if($_POST['operation'] == 'minus')
+						{
+							$total = $_POST['number1'] - $_POST['number2'];	
+						}
+						if($_POST['operation'] == 'times')
+						{
+							$total = $_POST['number1'] * $_POST['number2'];	
+						}
+						if($_POST['operation'] == 'divided by')
+						{
+							$total = $_POST['number1'] / $_POST['number2'];	
+						}
+						
+						// Print total to the browser
+						echo "<h1>{$_POST['number1']} {$_POST['operation']} {$_POST['number2']} equals {$total}</h1>";
+					
+					} else {
+						
+						// Print error message to the browser
+						echo 'Numeric values are required';
+					
+					}
+				}
+			
+			?>
+		    
+		    <!-- Calculator form -->
+		    <form method="post" action="calculator.php">
+		        <input name="number1" type="text" class="form-control" style="width: 150px; display: inline" />
+		        <select name="operation">
+		        	<option value="plus">Plus</option>
+		            <option value="minus">Minus</option>
+		            <option value="times">Times</option>
+		            <option value="divided by">Divided By</option>
+		        </select>
+		        <input name="number2" type="text" class="form-control" style="width: 150px; display: inline" />
+		        <input name="submit" type="submit" value="Calculate" class="btn btn-primary" />
+		    </form>
+	    
+		</div>
 	
-	 position: absolute;
-     left: 5px;
-}*/
-
-div {
-    width: 100px;
-    height: 50px;
-    background-color: red;
-    font-weight: bold;
-    position: relative;
-	/*position: absolute;*/
-    left: 5px;
-}
-
-/* Safari 4.0 - 8.0 */
-#div1 {-webkit-animation-timing-function: linear;}
-#div2 {-webkit-animation-timing-function: ease;}
-#div3 {-webkit-animation-timing-function: ease-in;}
-#div4 {-webkit-animation-timing-function: ease-out;}
-#div5 {-webkit-animation-timing-function: ease-in-out;}
-
-/* Standard syntax */
-#div1 {animation-timing-function: linear;}
-#div2 {animation-timing-function: ease;}
-#div3 {animation-timing-function: ease-in;}
-#div4 {animation-timing-function: ease-out;}
-#div5 {animation-timing-function: ease-in-out;}
-
-/* Safari 4.0 - 8.0 */
-@-webkit-keyframes mymove {
-    from {left: 0px;}
-    to {left: 300px;}
-}
-
-/* Standard syntax */
-@keyframes mymove {
-    from {left: 0px;}
-    to {left: 300px;}
-}
-</style>
-</head>
-<body>
-
-<p><strong>Note:</strong> The animation-timing-funtion property is not supported in Internet Explorer 9 and earlier versions.</p>
-
-<!--<div id="div1">linear</div>
-<div id="div2">ease</div>
-<div id="div3">ease-in</div>
-<div id="div4">ease-out</div>
-<div id="div5">ease-in-out</div>-->
-
-<div id="get">
-		<form method="post" action="BasicCalculator.php">
-<div id="group">        
-<div id="div1">Enter 1st number: <input type="text" name="number1" value="<?php if(isset($num1)) {echo $num1;} ?> " > </div></br> </br>
-        <div id="div2">Enter 2nd number: <input type="text" name="number2"  value="<?php if(isset($num2)) {echo $num2;} ?> " ></div> </br> </br>
-Select Operation: </br></br>
-                        <div id="div3"><input type="radio" name="g" value="+"> Addition</div> <br/>
-                        <div id="div4"><input type="radio" name="g" value="-"> Subtraction</div></br>
-                        <div id="div1"><input type="radio" name="g" value="*">Multiplication</div> <br/>
-                        <div id="div2"><input type="radio" name="g" value="/">Division</div> </br>
-                        <div id="div3"><input type="radio" name="g" value="%">Modulo</div> <br/><br/>
-                        <div id="div4"><input type="submit" name="submit"></div><br/><br/>
-                        <div id="div5"><h2 style="color:blue"> Output: <?php echo $result; ?> </h2></div>
-        </div>
-</form>
-
-</div>
-
-
-<script>
-//Your Javascript Code
-
-for(let i =5; i < window.width(); i++)
-{
-    document.getElementById('YOURELEMENT').left = i + 'px';
-}
-
-</script
-
-
-
-</body>
+	</body>
 </html>
